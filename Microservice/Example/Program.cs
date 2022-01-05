72de9754b744f489f90cd8016d2cd495224d0e78
Pavel <kulbida@hotmail.com>
2022-01-05 19:56:36 +0600

    beautifier

--- Microservice/Example/Program.cs
+++ Microservice/Example/Program.cs
@@ -3,7 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+string connectionString = builder
+  .Configuration
+  .GetConnectionString("DefaultConnection");
 
 
 // Add services to the container.
